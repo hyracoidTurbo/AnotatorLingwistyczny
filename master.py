18dd87e61ba1ac5f4937e86ec357a5fd087c5103
Kamil Kopacewicz <41971325+vaporizedcamel@users.noreply.github.com>
2019-06-04 23:54:23 +0200

    Update master.py

--- master.py
+++ master.py
@@ -136,7 +136,7 @@ class annotatorApp(tk.Tk):
                 opis = self.tags.find('./family[@name="'+ self.currentFamily +'"]/tag[name="'+self.tagList.get(self.tagList.curselection()) +'"]/description').text
                 opis += "\n"
                 opis += self.tags.find('./family[@name="'+ self.currentFamily +'"]/tag[name="'+self.tagList.get(self.tagList.curselection()) +'"]/literature').text
-                opis += "\n"
+                opis += "\nPrzykład:\n"
                 opis += self.tags.find('./family[@name="'+ self.currentFamily +'"]/tag[name="'+self.tagList.get(self.tagList.curselection()) +'"]/example').text
                 self.opisBox.delete('1.0', "end")
                 self.opisBox.insert(tk.END, opis)
